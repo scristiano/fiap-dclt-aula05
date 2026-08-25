15d27d7ef6644156fa503c8db34689d942df8cec
scristiano <Opa131961>
2026-08-25 00:30:48 +0000

    Adicionando Terraform CI

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -16,7 +16,7 @@ terraform {
   # Backend S3 para armazenar o state remotamente
   # IMPORTANTE: Criar bucket antes de usar
   backend "s3" {
-    bucket  = "fiap-terraform-state-dev"      # Bucket S3 para state
+    bucket  = "fiap-terraform-state-dev-066068186536"      # Bucket S3 para state
     key     = "development/terraform.tfstate" # Caminho do arquivo state
     region  = "us-east-1"                     # Região AWS
     encrypt = true                            # Criptografar state
